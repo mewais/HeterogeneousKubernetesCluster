3560737a76b1de4822ac26b11b2ae747e049494a
Mohammad Ewais <mohammad.a.ewais@gmail.com>
2020-04-12 14:25:06 -0400

    Remove unneeded package installation from workers

--- worker_setup.sh
+++ worker_setup.sh
@@ -5,7 +5,7 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
-sudo apt update && sudo apt install -y kubeadm kubelet docker.io keepalived
+sudo apt update && sudo apt install -y kubeadm kubelet docker.io
 
 # Enable Docker service
 sudo systemctl enable docker.service
